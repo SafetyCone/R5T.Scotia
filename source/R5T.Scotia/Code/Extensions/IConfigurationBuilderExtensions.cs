e2cc663328bf04f245dce6f7ef4310291b8e5e9d
MinexAutomation <minex.automation@gmail.com>
2019-12-08 22:53:50 -0800

    Moved configuration of a dependency injection container to a services collection extension method.

--- source/R5T.Scotia/Code/Extensions/IConfigurationBuilderExtensions.cs
+++ source/R5T.Scotia/Code/Extensions/IConfigurationBuilderExtensions.cs
@@ -3,17 +3,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-using R5T.Alamania;
-using R5T.Alamania.Bulgaria;
-using R5T.Bulgaria;
-using R5T.Bulgaria.Default.Local;
-using R5T.Costobocia;
-using R5T.Costobocia.Default;
-using R5T.Lombardy;
+using R5T.Scotia.Extensions;
+
 using R5T.Suebia;
-using R5T.Suebia.Alamania;
-using R5T.Visigothia;
-using R5T.Visigothia.Default.Local;
 
 
 namespace R5T.Scotia
@@ -27,17 +19,7 @@ namespace R5T.Scotia
         {
             // Build a service provider.
             var serviceProvider = new ServiceCollection()
-                .AddSingleton<IStringlyTypedPathOperator, StringlyTypedPathOperator>()
-
-                .AddSingleton<IUserProfileDirectoryPathProvider, DefaultLocalUserProfileDirectoryPathProvider>()
-                .AddSingleton<IDropboxDirectoryPathProvider, DefaultLocalDropboxDirectoryPathProvider>()
-                .AddSingleton<IOrganizationsStringlyTypedPathOperator, DefaultOrganizationsStringlyTypedPathOperator>()
-                .AddSingleton<IOrganizationDirectoryNameProvider, DefaultOrganizationDirectoryNameProvider>()
-                .AddSingleton<IOrganizationStringlyTypedPathOperator, DefaultOrganizationStringlyTypedPathOperator>()
-                .AddSingleton<IRivetOrganizationDirectoryPathProvider, BulgariaRivetOrganizationDirectoryPathProvider>()
-                .AddSingleton<ISecretsDirectoryPathProvider, AlamaniaSecretsDirectoryPathProvider>()
-                .AddSingleton<ISecretsFilePathProvider, AlamaniaSecretsFilePathProvider>()
-
+                .AddUserSecretFilesRivetLocation()
                 .BuildServiceProvider();
 
             // Get the secrets file path provider.
---
+++ source/R5T.Scotia/Code/Extensions/IServicesCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Alamania;
+using R5T.Alamania.Bulgaria;
+using R5T.Bulgaria;
+using R5T.Bulgaria.Default.Local;
+using R5T.Costobocia;
+using R5T.Costobocia.Default;
+using R5T.Lombardy;
+using R5T.Suebia;
+using R5T.Suebia.Alamania;
+using R5T.Visigothia;
+using R5T.Visigothia.Default.Local;
+
+
+namespace R5T.Scotia.Extensions
+{
+    public static class IServicesCollectionExtensions
+    {
+        public static IServiceCollection AddUserSecretFilesRivetLocation(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<IStringlyTypedPathOperator, StringlyTypedPathOperator>()
+
+                .AddSingleton<IUserProfileDirectoryPathProvider, DefaultLocalUserProfileDirectoryPathProvider>()
+                .AddSingleton<IDropboxDirectoryPathProvider, DefaultLocalDropboxDirectoryPathProvider>()
+                .AddSingleton<IOrganizationsStringlyTypedPathOperator, DefaultOrganizationsStringlyTypedPathOperator>()
+                .AddSingleton<IOrganizationDirectoryNameProvider, DefaultOrganizationDirectoryNameProvider>()
+                .AddSingleton<IOrganizationStringlyTypedPathOperator, DefaultOrganizationStringlyTypedPathOperator>()
+                .AddSingleton<IRivetOrganizationDirectoryPathProvider, BulgariaRivetOrganizationDirectoryPathProvider>()
+                .AddSingleton<ISecretsDirectoryPathProvider, AlamaniaSecretsDirectoryPathProvider>()
+                .AddSingleton<ISecretsFilePathProvider, AlamaniaSecretsFilePathProvider>()
+                ;
+
+            return services;
+        }
+    }
+}
